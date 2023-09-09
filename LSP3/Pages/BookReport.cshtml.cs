7a00519a1d4c865ced9588f539415fe577ce6875
markmcwhirter <mark.mcwhirter@gmail.com>
2023-09-09 07:58:08 -0700

    Add BookReport, BulkSalesEntry

--- LSP3/Pages/BookReport.cshtml.cs
+++ LSP3/Pages/BookReport.cshtml.cs
@@ -1,9 +1,6 @@
 using LSP3.Model;
 
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-using static System.Reflection.Metadata.BlobBuilder;
 
 namespace LSP3.Pages
 {
@@ -19,32 +16,54 @@ namespace LSP3.Pages
             _logger = logger;
         }
 
-        public async Task<IActionResult> OnGet()
-        {
-            BookSaleData = new List<BookSale>();
+        //public async Task<IActionResult> OnGet()
+        //{
+        //    BookSaleData = new List<BookSale>();
 
-            HttpHelper helper = new HttpHelper();
-            Extensions<List<BookSale>> bsExtention = new Extensions<List<BookSale>>();
+        //    HttpHelper helper = new HttpHelper();
+        //    Extensions<List<BookSale>> bsExtention = new Extensions<List<BookSale>>();
 
-            try
-            {
+        //    try
+        //    {
 
-                if (!base.IsAuthenticated)
-                    return Redirect("/Account/Login");
+        //        if (!base.IsAuthenticated)
+        //            return Redirect("/Account/Login");
 
-                string apiResponse = await helper.Get($"http://localhost:5253/api/sale/getsales");
+        //        string apiResponse = await helper.Get($"http://localhost:5253/api/sale/getsales");
 
-                if (!string.IsNullOrEmpty(apiResponse))
-                    BookSaleData = bsExtention.Deserialize(apiResponse);
+        //        if (!string.IsNullOrEmpty(apiResponse))
+        //        {
+        //            var temp = bsExtention.Deserialize(apiResponse);
+        //            BookSaleData = temp
+        //                .OrderBy(x => x.Author)
+        //                .ThenBy(y => y.Title)
+        //                .ThenBy(z => z.SaleID)
+        //                .ToList();
 
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Error: {ex.Message}: {ex.InnerException}: {ex.StackTrace}");
-            }
+        //            string oldauthor = "";
 
-            return Page();
+        //            foreach( var item in BookSaleData)
+        //            {
+        //                if (item.Author.Trim() == ",")
+        //                    item.Author = "(None)";
 
-        }
+        //                if (item.Author.Trim() != oldauthor)
+        //                    oldauthor = item.Author;
+        //                else
+        //                    item.Author = "";
+
+        //                item.SalesDate = item.SalesDate.Replace(" 00:00:00", "");
+        //            }
+        //        }
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError($"Error: {ex.Message}: {ex.InnerException}: {ex.StackTrace}");
+        //    }
+
+        //    return Page();
+
+        //}
     }
 }
